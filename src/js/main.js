d77df3b02bf9f4ecd1d009d17c9a42db2192d3b3
Daniel Abad Noriega <daniel.abad.noriega@Macmini.local>
2022-10-11 17:14:54 +0200

    Iteration: updated

--- src/js/main.js
+++ src/js/main.js
@@ -12,30 +12,5 @@ function dropdown() {
       options.style.marginLeft = "200px";
       options.classList.add = "fadeOut";
     }
-
-    /* if (
-      options.style.visibility !== "hidden" &&
-      options.style.visibility !== ""
-    ) {
-      options.classList.add = "fadeOut";
-      let fadeOut = setTimeout(() => {
-        options.style.visibility = "hidden";
-      }, 2000);
-    } else {
-      options.style.visibility = "visible";
-
-      options.classList.add = "fadeIn";
-    } */
   }
-}
-
-function showSkill(skill) {
-  if (document.getElementsByClassName("card-skill")[0] !== undefined) {
-    document.getElementsByClassName("card-skill")[0].remove();
-  }
-  console.log(skill);
-  const info = document.getElementById("aboutme");
-  const card = document.createElement("div");
-  card.classList.add("card-skill");
-  info.append(card);
 }
